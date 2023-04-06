13d04ff5ef68e5ce21df8c5a784beda5a9667b4a
TasleemtheArmy <taslu5678@gmail.com>
2023-04-06 10:51:04 +0530

    updated deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,8 +2,8 @@ echo '**************Enter deploy*************'
 
 npm run build
 
-npm start &
-sleep 1
+npm start #&
+# sleep 1
 echo $! > .pidfile
 
 echo '**************Deployment successful*********'
