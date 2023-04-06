506df896c0944b0c3da2b24ce64dddbbd5064745
TasleemtheArmy <93210325+Tasleem-Tabassum@users.noreply.github.com>
2023-04-06 11:01:19 +0530

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,9 +1,12 @@
 echo '**************Enter deploy*************'
 
+set -x
 npm run build
+set +x
 
+set-x
 npm start #&
 # sleep 1
 echo $! > .pidfile
-
+set +x
 echo '**************Deployment successful*********'
